82c1e2f2d02d4fe41229b3bbe5ccafe755de5626
OpenShift guest <topherchristie@gmail.com>
2013-12-19 17:24:19 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,83 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use('/bootstrap',express.static(path.resolve(__dirname, 'bower_components/bootstrap/dist')));
+router.use('/jquery',express.static(path.resolve(__dirname, 'bower_components/jquery')));
+router.use(express.static(path.resolve(__dirname, 'client')));
+var messages = [];
+var sockets = [];
+
+io.on('connection', function (socket) {
+    messages.forEach(function (data) {
+      socket.emit('addAccount', data);
+    });
+
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+      updateRoster();
+    });
+
+    socket.on('addAccount', function (account) {
+    
+      if (!account)
+        return;
+
+      socket.get('user', function (err, name) {
+        account.user = name;
+
+        broadcast('addAccount', account);
+        messages.push(account);
+      });
+    });
+
+    socket.on('identify', function (name) {
+      socket.set('user', String(name || 'Anonymous'), function (err) {
+        updateRoster();
+      });
+    });
+  });
+
+function updateRoster() {
+  async.map(
+    sockets,
+    function (socket, callback) {
+      socket.get('name', callback);
+    },
+    function (err, names) {
+      broadcast('roster', names);
+    }
+  );
+}
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    console.log('other server',process.env.PORT);
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
